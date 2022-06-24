9eb455c2181668d28b53552ee36d445307bd324b
Nwokeji Ikenna <105325845+Prinxey@users.noreply.github.com>
2022-06-24 11:06:39 +0100

    First task on JavaScript

---
+++ script.js
@@ -0,0 +1,7 @@
+let fname = "Nwokeji Ikenna Emmanuel";
+let height = "5.6";
+let country = "Nigeria";
+
+let info =
+  "Hello, my name is " + fname + ", " + height + " and I am from " + country;
+alert(info);
